d5750e6bc35b1e351cd2d20e7a5ead78f55fbe7b
Vikram Waradpande <wvikram11@gmail.com>
2017-06-26 13:13:06 +0530

    Begin to merge both files into one

---
+++ merge.py
@@ -0,0 +1,137 @@
+from tkinter import *
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+
+a = [0 for x in range(14)] #Stores textbox variables
+sigma=0
+
+# Functions -----------------------------------------------------------------
+def display():   #
+    t = int(num.get())
+    for i in range(0,2*t,2):
+        tk.Label(mainframe, text="X"+str((i+2)//2)+" Y"+str((i+2)//2)).grid(column=1,row=i+8,sticky=W)
+        a[i] = tk.Entry(mainframe, width=15)
+        a[i].grid(column=2, row=(8+i), sticky=(W,E))
+        a[i+1] = tk.Entry(mainframe, width=15)
+        a[i+1].grid(column=3, row=(8+i), sticky=(W,E))
+    tk.Button(mainframe, text="Process Data", command=processData).grid(column=2, row=3+len(a), sticky=W)
+
+
+
+# This function creates a new window for computations--------------------------------------------------------------
+def processData():
+    global a
+    global filename
+    print("Hello!")
+    newframe=tk.Toplevel(root)
+    calculate()
+    for child in newframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+
+
+#-------------------------------------------------------------------------------------------------------------------
+
+
+def calculate(*args):
+    global sigma
+    try:
+        freq = float(frequency.get())
+        len = float(length.get())
+        c=3*(10**8)
+        freq=freq*(10**6)
+        lambd=c/freq
+        num = (4*3.14159)*(len**4)
+        den = 3*(lambd**2)
+        sigma=(num/den)
+        print(sigma)
+    except ValueError:
+        pass
+
+
+def selectFile():
+    global filename
+    #print(filename)
+    filename = askopenfilename()
+    print(filename)
+    tk.Label(mainframe, text=filename+" selected", width=15).grid(column=2, row=1, sticky=W)
+
+#-----------------------------------------------------------------------------------------------------------
+
+#----------------Headers------------------------
+
+root = Tk()
+root.title("Calculation of Calibration Constant")
+mainframe = tk.Frame(root)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+#----------------------------------------------------
+
+
+#---------------Variables----------------------------
+
+feet = StringVar()
+meters = StringVar()
+length = StringVar()
+frequency = StringVar()
+pagr = StringVar()
+alpha = StringVar()
+num=StringVar()
+
+#----------------------------------------------------
+
+#Entries-------------TextFields and Grids----------------------
+pagr_entry = tk.Entry(mainframe, width=7, textvariable=pagr)
+length_entry = tk.Entry(mainframe, width=7, textvariable=length)
+frequency_entry = tk.Entry(mainframe, width=7, textvariable=frequency)
+alpha_entry = tk.Entry(mainframe, width=7, textvariable=alpha)
+number_entry = tk.Entry(mainframe, width=7, textvariable=num)
+
+#For snappy--------
+
+
+#Grids for entries
+frequency_entry.grid(column=2, row=3, sticky=(W,E))
+pagr_entry.grid(column=2, row=5, sticky=(W, E))
+alpha_entry.grid(column=2, row=6, sticky=(W, E))
+length_entry.grid(column=2, row=2, sticky=(W, E))
+number_entry.grid(column=2, row=7, sticky=(W,E))
+
+#---------------------------------------------------------
+
+
+
+#Widgets------------Contains Labels and Buttons------------------------------------
+tk.Button(mainframe, text="Select Dataset", command=selectFile).grid(column=1, row=1, sticky=W)
+
+tk.Label(mainframe, text="Length of Side").grid(column=1, row=2, sticky=W)
+tk.Label(mainframe, text="Meters").grid(column=3, row=2, sticky=W)
+
+tk.Label(mainframe, text="Frequency in MHz").grid(column=1, row=3, sticky=W)
+tk.Label(mainframe, text="MHz").grid(column=3, row=3, sticky=W)
+
+#tk.Button(mainframe, text="Calculate RCS", command=calculate).grid(column=1, row=4, sticky=E)
+#tk.Label(mainframe, textvariable=sigma).grid(column=3, row=4, sticky=(W, E))
+
+tk.Label(mainframe, text="Value of Pagr").grid(column=1, row=5, sticky=W)
+tk.Label(mainframe, text="in Metres^2").grid(column=3, row=5, sticky=W)
+
+tk.Label(mainframe, text="Alpha").grid(column=1, row=6, sticky=W)
+tk.Label(mainframe, text="Degrees").grid(column=3, row=6, sticky=W)
+
+tk.Label(mainframe, text="Number of Ref").grid(column=1, row=7, sticky=W)
+tk.Button(mainframe, text="Confirm", command=display).grid(column=3, row=7, sticky=W)
+
+
+#------------------------------------------------------------------------------------
+
+
+# Padding for the window.
+for child in mainframe.winfo_children(): child.grid_configure(padx=15, pady=15)
+
+length_entry.focus()
+
+root.bind('<Return>', calculate)
+
+root.mainloop()
